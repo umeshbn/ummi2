6e0a966d33c711e5c61b015cae674a6e25bf911e
umeshbn <umeshbneelaguri.xworkz@gmail.com>
2021-12-21 16:44:50 +0530

    its tiger ummi

---
+++ Five.java
@@ -0,0 +1,18 @@
+class Five{
+	public static void main(String[] args){
+	
+  	double []tradingpoints={10000.11111111,20000.0000,22222.2222};
+         System.out.println(tradingpoints[0]);
+         System.out.println(tradingpoints[1]);
+	 System.out.println(tradingpoints[2]);
+	 System.out.println(tradingpoints[3]);
+         System.out.println(tradingpoints[4]);
+	 System.out.println(tradingpoints[5]);
+	 System.out.println(tradingpoints[6]);
+         System.out.println(tradingpoints[7]);
+	 System.out.println(tradingpoints[8]);
+	
+	
+	}
+}
+ 	
